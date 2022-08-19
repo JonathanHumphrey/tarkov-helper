1ec0389a9d5553c5f555e6943477ecf7558337ff
Jonathan Humphrey <humphreyj1999@gmail.com>
2022-08-19 10:33:52 -0500

    initial commit

---
+++ src/components/CardGallery.vue
@@ -0,0 +1,34 @@
+<script setup>
+import ItemCard from "./ItemCard.vue";
+</script>
+
+<template>
+  <div class="card-wrapper">
+    <ItemCard>
+      <template v-slot:header="slotProps">
+        <h2 data-cy="card-prop-header">{{ slotProps.header }}</h2>
+      </template>
+      <template v-slot:body="{ image }">
+        <img data-cy="card-prop-body" :src="image" alt="" />
+      </template>
+      <template v-slot:footer="slotProps">
+        <p data-cy="card-prop-footer">{{ slotProps.footer }}</p>
+      </template>
+    </ItemCard>
+  </div>
+</template>
+
+<style scoped>
+.card-wrapper {
+  height: auto;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 5rem;
+}
+img {
+  width: 250px;
+  height: 250px;
+  align-self: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import CardGallery from "../components/CardGallery.vue";
+</script>
+
+<template>
+  <div class="home">
+    <CardGallery />
+  </div>
+</template>
+
+<style scoped>
+.home {
+  margin: auto;
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
