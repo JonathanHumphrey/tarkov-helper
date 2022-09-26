6dba1b18576d1994b9b796b1b886f0521215966d
Jonathan Humphrey <humphreyj1999@gmail.com>
2022-09-26 15:02:05 -0500

    began laying out the crafting calculator

--- src/components/CraftingCard.vue
+++ src/components/CraftingCard.vue
@@ -10,6 +10,26 @@
 <template>
     <div class="card" v-for="station in hideoutStations" :key="station.name">
     <slot name="header" :header="station.name"></slot>
-    
+        <div class="crafts" v-for="craft in station.crafts">
+            <slot name="body" :body="craft.rewardItems[0].item.name"></slot>
+            <div class="reqItems" v-for="item in craft.requiredItems">
+                <slot name="reqItems" :object="item.name" :quantity="item.quantity"></slot>
+            </div>
+        </div>
     </div>
 </template>
+<style scoped>
+.card {
+  color: rgb(219, 192, 69);
+  box-shadow: 0 4px 8px 0 rgba(255, 255, 255, 0.2);
+  transition: 0.3s;
+  margin-bottom: 20px;
+  border-radius: 2rem;
+  background: #434547;
+  width: 25rem;
+  padding: 2rem;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(255, 255, 255, 0.2);
+}
+</style>
--- src/components/CraftingGallery.vue
+++ src/components/CraftingGallery.vue
@@ -10,8 +10,25 @@ import { storeToRefs } from "pinia";
         <template v-slot:header="slotProps">
             <h2 data-cy="card-prop-header">{{ slotProps.header }}</h2>
         </template>
-        
-        
+        <template v-slot:body="slotProps">
+            <p> {{slotProps.body}}</p>
+        </template>  
+        <template v-slot:reqItems="slotProps">
+            <p>{{slotProps.object}}</p>
+            <p>x{{slotProps.quantity}}</p>
+        </template>
     </CraftingCard>
     </div>
 </template>
+
+<style scoped>
+.card-wrapper {
+  width: 60rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  margin-top: 5rem;
+  min-height: 100vh;
+}
+</style>
